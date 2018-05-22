56704324cfb7bf16711f36a380133dce969f4d0f
yasu.miyazaki <kizayami.yasu@gmail.com>
2018-05-22 04:00:32 +0000

    want revised

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,5 +1,6 @@
 class OwnershipsController < ApplicationController
   def create
+
     @item = Item.find_or_initialize_by(code: params[:item_code])
 
     unless @item.persisted?
@@ -14,19 +15,28 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+    
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました'
+      
     end
-
+    
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
+
     @item = Item.find(params[:item_id])
 
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item)
+      flash[:success] = '商品のHave を解除しました。'
     end
-
     redirect_back(fallback_location: root_path)
   end
 end
+
